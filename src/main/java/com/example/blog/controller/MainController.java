c0e066143a6d7de3ec80905080028b3adab8a82a
jaewoo2233 <ta3844@naver.com>
2023-04-19 23:34:43 +0900

    MODIFY: HTML 기본적인 툴 추가

---
+++ src/main/java/com/example/blog/controller/MainController.java
@@ -0,0 +1,52 @@
+package com.example.blog.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+
+
+    @GetMapping("/joinForm")
+    public String joinForm() {
+        return "user/joinForm";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "user/loginForm";
+    }
+
+    @GetMapping("/user/updateForm")
+    public String userUpdateForm() {
+        return "user/updateForm";
+    }
+
+    @GetMapping({ "/", "/board" })
+    public String main() {
+        return "board/main";
+    }
+
+    @GetMapping("/board/{id}")
+    public String boardDetail() {
+        return "board/detail";
+    }
+
+    @GetMapping("/board/saveForm")
+    public String boardSaveForm() {
+        return "board/saveForm";
+    }
+
+    @GetMapping("/board/{id}/updateForm")
+    public String boardUpdateForm() {
+        return "board/updateForm";
+    }
+
+    @GetMapping("/user/profileUpdateForm")
+    public String profileUpdateForm(){
+        return "user/profileUpdateForm";
+    }
+}
